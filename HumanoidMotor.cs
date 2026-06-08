9db940e7f61cd01f54646e27ed51e8818419e787
justawhitehacker <75467556+justawhitehacker@users.noreply.github.com>
2026-06-08 17:10:17 +0700

    remove unused and accident-to-add lib, UnityEngine.Rendering

--- HumanoidMotor.cs
+++ HumanoidMotor.cs
@@ -1,5 +1,4 @@
 using System;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 [Serializable, RequireComponent(typeof(Humanoid)), RequireComponent(typeof(Rigidbody)), RequireComponent(typeof(Collider))]
